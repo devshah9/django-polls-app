26a2ba682278dac37a5e7b58637db2770374cbf8
dev <devshah972@gmail.com>
2021-01-28 20:19:19 +0530

    Update how name, date, vote and search fuction in view. Update url

--- polls/views.py
+++ polls/views.py
@@ -12,26 +12,25 @@ from django.http import HttpResponse
 def polls_list(request):
     all_polls = Poll.objects.all()
     search_term = ''
-    if 'name' in request.GET:
+    if request.GET.get('name') == 'True':
         all_polls = all_polls.order_by('text')
-
-    if 'date' in request.GET:
+    
+    if request.GET.get('date') == 'True':
         all_polls = all_polls.order_by('pub_date')
-
-    if 'vote' in request.GET:
+    
+    if request.GET.get('vote') == 'True':
         all_polls = all_polls.annotate(Count('vote')).order_by('vote__count')
-
+    
     if 'search' in request.GET:
         search_term = request.GET['search']
         all_polls = all_polls.filter(text__icontains=search_term)
 
-    paginator = Paginator(all_polls, 6)  # Show 6 contacts per page
+    paginator = Paginator(all_polls, 7)  # Show 7 contacts per page
     page = request.GET.get('page')
     polls = paginator.get_page(page)
 
     get_dict_copy = request.GET.copy()
     params = get_dict_copy.pop('page', True) and get_dict_copy.urlencode()
-    print(params)
     context = {
         'polls': polls,
         'params': params,
@@ -43,8 +42,22 @@ def polls_list(request):
 @login_required()
 def list_by_user(request):
     all_polls = Poll.objects.filter(owner=request.user)
-    paginator = Paginator(all_polls, 7)  # Show 7 contacts per page
+    search_term = ''
+    if request.GET.get('name') == 'True':
+        all_polls = all_polls.order_by('text')
+    
+    if request.GET.get('date') == 'True':
+        all_polls = all_polls.order_by('pub_date')
+    
+    if request.GET.get('vote') == 'True':
+        all_polls = all_polls.annotate(Count('vote')).order_by('vote__count')
+    
+    if 'search' in request.GET:
+        search_term = request.GET['search']
+        all_polls = all_polls.filter(text__icontains=search_term)
 
+        
+    paginator = Paginator(all_polls, 7)  # Show 7 contacts per page
     page = request.GET.get('page')
     polls = paginator.get_page(page)
 
